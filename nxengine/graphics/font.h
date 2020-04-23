9b7bec7a4e8e47db3601abcec5e42c1a4139fd14
JooYoung <sana2dang@naver.com>
2020-04-23 15:50:18 +0900

    한글 지원 패치
    브리티쉬 님의 한글지원 패치
    https://blog.naver.com/british2/221924557853
    
    비트맵 폰트 12 x 12

--- nxengine/graphics/font.h
+++ nxengine/graphics/font.h
@@ -6,6 +6,9 @@
 #define NUM_LETTERS_RENDERED	256 // Allow usage of the other half of the font, containing the diacritics
 #define FONT_DEFAULT_SPACING	5
 
+#define NUM_FONT_KR_LETTERS		2350
+#define NUM_KR_LETTERS_RENDERED	2350
+
 class NXFont
 {
 public:
@@ -13,16 +16,19 @@ public:
 	~NXFont();
 	bool InitChars(SDL_Surface *font, uint32_t color);
 	bool InitCharsShadowed(SDL_Surface *top, uint32_t color, uint32_t shadowcolor);
+	bool InitKrChars(SDL_Surface *font, uint32_t color);
+	bool InitKrCharsShadowed(SDL_Surface *top, uint32_t color, uint32_t shadowcolor);
 	void free();
 	
 	SDL_Surface *letters[NUM_FONT_LETTERS];
+	SDL_Surface *krletters[NUM_FONT_KR_LETTERS];
 };
 
 
 extern NXFont whitefont;
-extern NXFont greenfont;
+//extern NXFont greenfont;
 extern NXFont bluefont;		// used for "F3:Options" text on pause screen
-extern NXFont shadowfont;	// white letters w/ drop shadow
+//extern NXFont shadowfont;	// white letters w/ drop shadow
 
 bool font_init(void);
 void font_close(void);
