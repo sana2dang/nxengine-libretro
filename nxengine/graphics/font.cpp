54800f105dfefa34b6a106788061343bddfefd26
JooYoung <sana2dang@naver.com>
2020-04-23 14:43:45 +0900

    한글지원 패치
    브리티쉬 님의 한글지원 패치
    https://blog.naver.com/british2/221924557853

--- nxengine/graphics/font.cpp
+++ nxengine/graphics/font.cpp
@@ -35,31 +35,38 @@ static bool shrink_spaces = true;
 static int fontheight = 0;
 
 NXFont whitefont;
-NXFont greenfont;
+//NXFont greenfont;
 NXFont bluefont;		// used for "F3:Options" text on pause screen
-NXFont shadowfont;		// white letters w/ drop shadow
+//NXFont shadowfont;		// white letters w/ drop shadow
 
 #include "../libretro/bitmap_font.h"
 
 bool font_init(void)
 {
 	bool error = false;
-   LRSDL_RWops *rw = LRSDL_RWFromMem(font_bmp, sizeof(font_bmp));
+    LRSDL_RWops *rw = LRSDL_RWFromMem(font_bmp, sizeof(font_bmp));
+    LRSDL_RWops *krrw = LRSDL_RWFromMem(kr_font_bmp, sizeof(kr_font_bmp));
 
 	// we'll be bypassing the NXSurface automatic scaling features
 	// and drawing at the real resolution so we can get better-looking fonts.
 	sdl_screen = screen->fSurface;
 
 	SDL_Surface *font = LRSDL_LoadBMP_RW(rw, 1);
+	SDL_Surface *krfont = LRSDL_LoadBMP_RW(krrw, 1);
 	SetColorKey(font, SDL_SRCCOLORKEY, 0);
+	SetColorKey(krfont, SDL_SRCCOLORKEY, 0);
 
 	error |= whitefont.InitChars(font, 0xffffff);
-	error |= greenfont.InitChars(font, 0xffffff); // Workaround for avoiding diacritics to show in green color
+	error |= whitefont.InitKrChars(krfont, 0xffffff);
+//	error |= greenfont.InitChars(font, 0xffffff); // Workaround for avoiding diacritics to show in green color
 	error |= bluefont.InitCharsShadowed(font, 0xffffff, 0x000000); // Workaround for avoiding diacritics not showing on map location names
-	error |= shadowfont.InitCharsShadowed(font, 0xffffff, 0x000000);
+	error |= bluefont.InitKrCharsShadowed(krfont, 0xffffff, 0x000000); // Workaround for avoiding diacritics not showing on map location names
+//	error |= shadowfont.InitCharsShadowed(font, 0xffffff, 0x000000);
+	
 	error |= create_shade_sfc();
 
 	FreeSurface(font);
+	FreeSurface(krfont);
 
 	if (error) return 1;
 
@@ -76,6 +83,9 @@ bool font_init(void)
 			fontheight = whitefont.letters[c]->h;
 	}
 
+	if (whitefont.krletters[0]->h > fontheight)
+		fontheight = whitefont.krletters[0]->h;
+
 	initialized = true;
 	return 0;
 }
@@ -90,9 +100,9 @@ bool font_reload()
 		return 0;
 	
 	whitefont.free();
-	greenfont.free();
+//	greenfont.free();
 	bluefont.free();
-	shadowfont.free();
+//	shadowfont.free();
 	
 	return font_init();
 }
@@ -104,6 +114,7 @@ void c------------------------------() {}
 NXFont::NXFont()
 {
 	memset(letters, 0, sizeof(letters));
+	memset(krletters, 0, sizeof(krletters));
 }
 
 NXFont::~NXFont()
@@ -118,6 +129,12 @@ void NXFont::free()
 		if (letters[i])
 			FreeSurface(letters[i]);
 		letters[i] = NULL;
+	}
+	for(int i=0;i<NUM_KR_LETTERS_RENDERED;i++)	// added kr letters
+	{
+		if (krletters[i])
+			FreeSurface(krletters[i]);
+		krletters[i] = NULL;
 	}
 }
 
@@ -143,13 +160,13 @@ bool NXFont::InitChars(SDL_Surface *font, uint32_t color)
 	fgcolor.g = (uint8_t)(color >> 8);
 	fgcolor.b = (uint8_t)(color);
 
-	char str[2];
-	str[1] = 0;
+	//char str[2];
+	//str[1] = 0;
 	uint16_t blue = 0x1f;
 
 	for(int i=1;i<NUM_LETTERS_RENDERED;i++)
 	{
-		str[0] = i;
+		//str[0] = i;
 
       letter = (SDL_Surface*)AllocNewSurface(0, 6, 10);
 
@@ -255,6 +272,118 @@ bool NXFont::InitCharsShadowed(SDL_Surface *font, uint32_t color, uint32_t shado
 	return 0;
 }
 
+bool NXFont::InitKrChars(SDL_Surface *font, uint32_t color)
+{
+	SDL_Color fgcolor;
+	SDL_Surface *krletter;
+
+	fgcolor.r = (uint8_t)(color >> 16);
+	fgcolor.g = (uint8_t)(color >> 8);
+	fgcolor.b = (uint8_t)(color);
+
+	uint16_t blue = 0x1f;
+
+	for(int i=0;i<NUM_KR_LETTERS_RENDERED;i++)
+	{
+
+		krletter = (SDL_Surface*)AllocNewSurface(0, 14, 14);	// han font size : 14 x 14
+
+		SDL_Rect src = {0};
+
+		src.w = 13;
+		src.h = 13;
+		src.x = (i % 9) * 13;
+		src.y = (i / 9) * 13;
+
+		SDL_Rect dst = {0};
+		dst.w = krletter->w;
+		dst.h = krletter->h;
+
+		SetColorKey(krletter, SDL_SRCCOLORKEY, blue);
+		FillRectangle(krletter, NULL, blue);
+
+		DrawBlit(font, &src, krletter, &dst);
+
+		uint16 color = fgcolor.r << RED_SHIFT
+		| fgcolor.g << GREEN_SHIFT
+		| fgcolor.b << BLUE_SHIFT;
+
+		set_color(krletter, color, blue);
+
+		krletters[i] = krletter;
+	}
+
+	return 0;
+}
+
+// create a font with a drop-shadow (used for "MNA" stage-name displays)
+bool NXFont::InitKrCharsShadowed(SDL_Surface *font, uint32_t color, uint32_t shadowcolor)
+{
+	SDL_Color fgcolor, bgcolor;
+	SDL_Surface *top, *bottom;
+	SDL_Rect dstrect;
+	const int offset = 2;
+
+	fgcolor.r = (uint8_t)(color >> 16);
+	fgcolor.g = (uint8_t)(color >> 8);
+	fgcolor.b = (uint8_t)(color);
+
+	bgcolor.r = (uint8_t)(shadowcolor >> 16);
+	bgcolor.g = (uint8_t)(shadowcolor >> 8);
+	bgcolor.b = (uint8_t)(shadowcolor);
+
+	for(int i=0;i<NUM_KR_LETTERS_RENDERED;i++)
+	{
+		uint16_t blue = 0x1f;
+
+		top = (SDL_Surface*)AllocNewSurface(0, 14, 14);
+		bottom = (SDL_Surface*)AllocNewSurface(0, 14, 14);
+
+		FillRectangle(top, NULL, blue);
+		FillRectangle(bottom, NULL, blue);
+		SetColorKey(top, SDL_SRCCOLORKEY, blue);
+		SetColorKey(bottom, SDL_SRCCOLORKEY, blue);
+
+		SDL_Rect src = {0};
+		src.w = 13;
+		src.h = 13;
+		src.x = (i % 9) * 13;
+		src.y = (i / 9) * 13;
+
+		SDL_Rect dst = {0};
+		dst.w = top->w;
+		dst.h = top->h;
+
+		DrawBlit(font, &src, top, &dst);
+		DrawBlit(font, &src, bottom, &dst);
+
+		uint16_t color_fg = fgcolor.r << RED_SHIFT
+		| fgcolor.g << GREEN_SHIFT
+		| fgcolor.b << BLUE_SHIFT;
+		uint16_t color_bg = bgcolor.r << RED_SHIFT
+		| bgcolor.g << GREEN_SHIFT
+		| bgcolor.b << BLUE_SHIFT;
+
+		set_color(top, color_fg, blue);
+		set_color(bottom, color_bg, blue);
+
+		krletters[i] = (SDL_Surface*)AllocNewSurface(0, top->w, top->h+offset);
+
+		SetColorKey(krletters[i], SDL_SRCCOLORKEY, blue);
+		FillRectangle(krletters[i], NULL, blue);
+
+		dstrect.x = 0;
+		dstrect.y = offset;
+		DrawBlit(bottom, NULL, krletters[i], &dstrect);
+
+		dstrect.x = 0;
+		dstrect.y = 0;
+		DrawBlit(top, NULL, krletters[i], &dstrect);
+	}
+
+	return 0;
+}
+
 /*
 void c------------------------------() {}
 */
@@ -269,37 +398,72 @@ static int text_draw(int x, int y, const char *text, int spacing, NXFont *font)
 	for(i=0;text[i];i++)
 	{
 		char ch = text[i];
-		SDL_Surface *letter = font->letters[ch];
-
-		if (ch == '=' && game.mode != GM_CREDITS)
+		char ch2 = text[i+1];
+		if (ch >= 0xB0 && ch <= 0xC8 && ch2 >= 0xA1 && ch2 <= 0xFE) // KSC5601 : 0xB0A1 ~ 0xC8FE (except xxA0, xxFF)
 		{
-			if (rendering)
-				draw_sprite((x), (y)+2, SPR_TEXTBULLET);
-		}
-		else if (rendering && ch != ' ' && letter)
-		{
-			// must set this every time, because SDL_BlitSurface overwrites
-			// dstrect with final clipping rectangle.
-			dstrect.x = x;
-			dstrect.y = y;
-			DrawBlit(letter, NULL, sdl_screen, &dstrect);
-		}
+			unsigned short krch = ((unsigned short)ch - 0xB0) * 94 + (ch2 - 0xA1);
+			SDL_Surface *krletter = font->krletters[krch];
 
-		if (spacing != 0)
-		{	// fixed spacing
-			x += spacing;
+			if (rendering && krletter)
+			{
+				// must set this every time, because SDL_BlitSurface overwrites
+				// dstrect with final clipping rectangle.
+				dstrect.x = x;
+				dstrect.y = y;
+				DrawBlit(krletter, NULL, sdl_screen, &dstrect);
+			}
+
+			if (krletter) 
+			{
+				if (krletter->w < spacing * 2)
+				{	// fixed spacing
+					x += spacing * 2;
+				}
+				else
+				{
+					x += krletter->w;
+				}
+				i++;
+			} 
+			else if (spacing != 0)
+			{
+				x += spacing;
+			}
 		}
-		else
-		{	// variable spacing
-			if (ch == ' ' && shrink_spaces)
-			{	// 10.5 px for spaces - make smaller than they really are - the default
-				x += 6;
-				if (i & 1) x++;
+		else 
+		{
+			SDL_Surface *letter = font->letters[ch];
+
+			if (ch == '=' && game.mode != GM_CREDITS)
+			{
+				if (rendering)
+					draw_sprite((x), (y)+2, SPR_TEXTBULLET);
+			}
+			else if (rendering && ch != ' ' && letter)
+			{
+				// must set this every time, because SDL_BlitSurface overwrites
+				// dstrect with final clipping rectangle.
+				dstrect.x = x;
+				dstrect.y = y;
+				DrawBlit(letter, NULL, sdl_screen, &dstrect);
+			}
+
+			if (spacing != 0)
+			{	// fixed spacing
+				x += spacing;
 			}
 			else
-			{
-				if (letter)
-					x += letter->w;
+			{	// variable spacing
+				if (ch == ' ' && shrink_spaces)
+				{	// 10.5 px for spaces - make smaller than they really are - the default
+					x += 6;
+					if (i & 1) x++;
+				}
+				else
+				{
+					if (letter)
+						x += letter->w;
+				}
 			}
 		}
 	}
